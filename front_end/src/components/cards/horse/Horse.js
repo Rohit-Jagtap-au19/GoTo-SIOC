fb62a6c3a9233e2f6688a9eee200f7a686bda716
Rohit <prjagtap90@gmail.com>
2023-01-10 17:47:31 +0530

    cards done

--- front_end/src/components/cards/horse/Horse.js
+++ front_end/src/components/cards/horse/Horse.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Card from '../card/Card'
 import './horse.scss'
 
 const Horse = () => {
   return (
     <div className='horse_card'>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
     </div>
   )
 }
--- front_end/src/components/home/Home.js
+++ front_end/src/components/home/Home.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import Card from '../cards/card/Card'
 import Horse from '../cards/horse/Horse'
 import Jockey from '../cards/jockey/Jockey'
 
 const Home = () => {
   return (
     <div className='cards'>
-    {/* <h1>Create Team</h1>
+    <h1>Create Team</h1>
     <h1>Choose Horse to Race</h1>
       <Horse/>
     <h1>Choose Jockey to Race</h1>
       <Jockey/>
-      <button>Continue</button> */}
-      <Card/>
+      <button>Continue</button>
     </div>
   )
 }
